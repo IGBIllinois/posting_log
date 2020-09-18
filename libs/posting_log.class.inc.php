7b08a3fba2f0f7f26b93ebb82c429393e8d1e7e2
root <root@dslater-lnx.igb.illinois.edu>
2020-09-18 10:10:47 -0500

    Started posting_log class

---
+++ libs/posting_log.class.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class posting_log {
+
+	private $db; 
+	
+	////////////////Public Functions///////////
+
+        public function __construct($db) {
+                $this->db = $db
+
+
+        }
+        public function __destruct() {
+
+        }
+
+
+
+
+
+
+
+}
+
+?>
