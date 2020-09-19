5f35caadfad7e00e8bda12e58007f8d6ac08848f
David Slater <dslater@illinois.edu>
2020-09-19 17:18:32 -0500

    Added twig to composer Started email digest support

--- libs/functions.class.inc.php
+++ libs/functions.class.inc.php
@@ -2,9 +2,12 @@
 
 class functions {
 
-	public static function get_log($db,$start=0,$count=0) {
+	public static function get_log($db,$start=0,$count=0,$start_date=0,$end_date=0) {
 
 		$sql = "SELECT * FROM posting_log ";
+		if (($start_date != 0) && ($end_date != 0 )) {
+			$sql .= "WHERE time_access BETWEEN CAST(" . $start_date . " AS DATE) AND CAST(" . $end_date . " AS DATE) ";
+		}
 		$sql .= "ORDER by time_access DESC ";
 		if ($count != 0) {
         	        $sql .= "LIMIT " . $start . "," . $count;
@@ -16,10 +19,6 @@ class functions {
 
 
 	}
-
-
-
-
 
 
 }
