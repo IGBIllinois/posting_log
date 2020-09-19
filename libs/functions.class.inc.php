9e6d6dd59338d46e6d9c16b4b9327907cdadd725
root <root@dslater-lnx.igb.illinois.edu>
2020-09-19 11:27:44 -0500

    removed unneeded settings.

--- libs/functions.class.inc.php
+++ libs/functions.class.inc.php
@@ -2,10 +2,14 @@
 
 class functions {
 
-	public static function get_log($db) {
+	public static function get_log($db,$start=0,$count=0) {
 
 		$sql = "SELECT * FROM posting_log ";
-		$sql .= "ORDER by time_access DESC";
+		$sql .= "ORDER by time_access DESC ";
+		if ($count != 0) {
+        	        $sql .= "LIMIT " . $start . "," . $count;
+	        }
+
 		return $result = $db->query($sql);	
 
 
