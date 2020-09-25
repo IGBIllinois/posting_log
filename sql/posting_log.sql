4511d0288bff0574a8bcfdf79c1785eba8123048
root <root@posting.biotech.illinois.edu>
2020-09-25 11:55:59 -0500

    Started work on scrapping for files that were uploaded

--- sql/posting_log.sql
+++ sql/posting_log.sql
@@ -1,4 +1,4 @@
-CREATE TABLE posting_log (
+CREATE TABLE downloads (
 	id INT NOT NULL AUTO_INCREMENT,
 	time_access DATETIME,
 	remote_ip VARCHAR(20),
@@ -12,3 +12,15 @@ CREATE TABLE posting_log (
 	UNIQUE unique_index(time_access,remote_ip,email,filename)
 ) ENGINE=InnoDB;
 
+CREATE TABLE uploads (
+	id INT NOT NULL AUTO_INCREMENT,
+	time_access DATETIME,
+	remote_ip VARCHAR(20),
+	remote_hostname VARCHAR(255),
+	filename VARCHAR(255),
+	useragent VARCHAR(255),
+	json TEXT NOT NULL,
+	success BOOLEAN,
+	PRIMARY KEY(id),
+	UNIQUE unique_index(time_access,remote_ip,filename)
+) ENGINE=InnoDB;
