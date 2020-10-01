cb7dbea274b50ad97b821d3ec15a3aef635f94ae
David Slater <dslater@illinois.edu>
2020-10-01 07:22:47 -0500

    Version 1.1

--- conf/app.inc.php
+++ conf/app.inc.php
@@ -2,7 +2,7 @@
 /*
 Stores application variables
 */
-define("VERSION","1.1Beta");
+define("VERSION","1.1");
 define("TWIG_DIR","templates");
 define("EMAIL_CSS","vendor/twbs/bootstrap/dist/css/bootstrap.min.css");
 ?>
